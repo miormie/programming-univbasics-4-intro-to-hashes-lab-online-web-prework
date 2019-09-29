92d0c64f917be96f2b3497b1378fa78ff88e4019
Rose Seraphin <roseseraphin@yahoo.com>
2019-09-29 23:48:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,13 +39,9 @@ def update_counting_hash(hash, key)
  if hash[key] 
    hash[key] += 1 
  else 
-   
-   
- 
- 
-  
-  
-  
+   hash[key] = 1 
+ end 
+  return hash 
   
   
   # given a hash an a key as parameters, return an updated hash
